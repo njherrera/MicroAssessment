6d9542ed7e94a254b9692cf628c25c19289948fa
njherrera <nathan.j.herrera@gmail.com>
2022-05-30 18:52:09 -0700

    very small commit so that i can add to the readme and pull to synch with remote repo

--- src/com/mucho/TreeCalculator.java
+++ src/com/mucho/TreeCalculator.java
@@ -1,4 +1,11 @@
 package com.mucho;
 
 public class TreeCalculator {
+
+    // since the tree was constructed from postfix notation, we'll be evaluating it using an inorder traversal
+    public double calculateTreeValue (Node root){
+        if (root != null){
+            
+        }
+    }
 }
